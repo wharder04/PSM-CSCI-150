5b9e3ead602876c64885566a482beba47e87a824
Prab999 <Prabhjeetaluna23@gmail.com>
2025-10-30 17:35:48 -0700

    updated app for new files, added button click functionality to forgot password page

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,10 +1,17 @@
-import React from "react";
 import "./ForgotPassword.css";
+import { useNavigate } from "react-router-dom";
 
 export default function ForgotPassword() {
+  const navigate = useNavigate();
 
 function handleResetButton() {
-  console.log("Reset Password button clicked");
+  navigate('/Verify');
+}
+function handleBackArrow() {
+  console.log("Back arrow clicked");
+}
+function handlebSignIn() {
+  console.log("Back to Sign In clicked");
 }
 
   return (
@@ -12,13 +19,13 @@ function handleResetButton() {
       <div className="forgot">
       <div className="left-side"></div>
       <div className="right-side">
-        <button className="back-button">←</button>
+        <button className="back-button" onClick={handleBackArrow}>←</button>
         <h1>Forgot<br />
         Your Password?</h1>
         <p> Email</p>
         <input type="email" placeholder="Enter your email" />
         <button onClick={handleResetButton}>Reset Password</button>
-        <button>Back to Sign In</button>
+        <button onClick={handlebSignIn}>Back to Sign In</button>
         </div>
       </div>
     </div>
