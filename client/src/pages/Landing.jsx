b1241ed5e516f9e8d9ccf75e21d12271caff1e9d
Prab999 <Prabhjeetaluna23@gmail.com>
2025-11-04 13:16:06 -0800

    Added button to go from landing to login

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Landing() {
+  const navigate = useNavigate();
+
+
   return (
     <>
       <h1 className="">Landing Page Here</h1>
+      <button onClick={() => navigate("/auth/login")}>Go to Login</button>
     </>
   );
 }
