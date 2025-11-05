933f4949243938d730b72a627e5e7212216ead00
Aleyah <leilei55@mail.fresnostate.edu>
2025-11-04 19:23:27 -0800

    Updated login and createAccount CSS file so that it scales to viewport

--- client/src/pages/AccountCreate.jsx
+++ client/src/pages/AccountCreate.jsx
@@ -16,35 +16,37 @@ function AccountCreate() {
 
   return (
     <>
-      <div className="createAccount">
-        <div id="leftSide">
-          <h2>Let's Get Started</h2>
-        </div>
-        <div id="rightSide">
-          <h2>Create Account</h2>
-          <p>Enter your details to create account</p>
-          <label for="fname">First name:</label>
-          <br />
-          <input type="text" id="fname" name="fname" />
-          <br />
-          <label for="lname">Last name:</label>
-          <br />
-          <input type="text" id="lname" name="lname" />
-          <br />
-          <label for="email">Email:</label>
-          <br />
-          <input type="text" id="email" name="email" />
-          <br />
-          <label for="password">Password:</label>
-          <br />
-          <input type="text" id="password" name="password" />
-          <br />
-          <button type="signup">Sign up</button>
-          <p>or</p>
-          <br />
-          <button>Google</button>
-          <p>Already have an account?</p>
-          <Link to="/auth/login">Login here</Link>
+      <div className ="createAccountPage">
+        <div className="createAccount">
+          <div id="leftSide">
+            <h2>Let's Get Started</h2>
+          </div>
+          <div id="rightSide">
+            <h2>Create Account</h2>
+            <p>Enter your details to create account</p>
+            <label for="fname">First name:</label>
+            <br />
+            <input type="text" id="fname" name="fname" />
+            <br />
+            <label for="lname">Last name:</label>
+            <br />
+            <input type="text" id="lname" name="lname" />
+            <br />
+            <label for="email">Email:</label>
+            <br />
+            <input type="text" id="email" name="email" />
+            <br />
+            <label for="password">Password:</label>
+            <br />
+            <input type="text" id="password" name="password" />
+            <br />
+            <button type="signup">Sign up</button>
+            <p>or</p>
+            <br />
+            <button>Google</button>
+            <p>Already have an account?</p>
+            <Link to="/auth/login">Login here</Link>
+          </div>
         </div>
       </div>
     </>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -16,26 +16,28 @@ function Login() {
 
   return (
     <>
-      <div className="login">
-        <div id="leftSide"></div>
-        <div id="rightSide">
-          <h2>Welcome Back</h2>
-          <h3>Login in to your account</h3>
-          <label for="username">Username:</label>
-          <br />
-          <input type="text" id="username" name="username" />
-          <br />
-          <label for="password">Password:</label>
-          <br />
-          <input type="text" id="password" name="password" />
-          <br />
-          <button type="submit">Sign in</button>
-          <p>
-            <Link to="/auth/forgot"> Forgot Password </Link>
-          </p>
-          <p>
-            Don't have an account? <Link to="/auth/register">Sign up</Link>
-          </p>
+      <div className="loginPage">
+        <div className="login">
+          <div id="leftSide"></div>
+          <div id="rightSide">
+            <h2>Welcome Back</h2>
+            <h3>Login in to your account</h3>
+            <label for="username">Username:</label>
+            <br />
+            <input type="text" id="username" name="username" />
+            <br />
+            <label for="password">Password:</label>
+            <br />
+            <input type="text" id="password" name="password" />
+            <br />
+            <button type="submit">Sign in</button>
+            <p>
+              <Link to="/auth/forgot"> Forgot Password </Link>
+            </p>
+            <p>
+              Don't have an account? <Link to="/auth/register">Sign up</Link>
+            </p>
+          </div>
         </div>
       </div>
     </>
