729e312863095f99cd7b8cc7efaf6aa043f0f195
Jahnavi Sharma <jahnaviruchi@gmail.com>
2025-11-04 17:34:52 -0800

    Code Refactor

--- jest.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.[jt]s"],
-  setupFilesAfterEnv: ["<rootDir>/tests/__utils__/env.ts"],
-  verbose: true,
-  maxWorkers: 1
-};
