1220ea9df17d4f551ede50705718a18f38a2f3a7
Prab999 <Prabhjeetaluna23@gmail.com>
2025-11-06 17:45:14 -0800

    Added design elements to Projects-page

--- client/src/pages/projects/ProjectsPage.jsx
+++ client/src/pages/projects/ProjectsPage.jsx
@@ -2,10 +2,18 @@ import "./Projects-Page.css";
 import DashboardLayout from "../Dashboard/DashboardLayout.jsx";
 
 export default function ProjectsPage() {
+  let name = "PSM Group";
   return (
     <DashboardLayout>
       <div className="projects-page">
-        <h1>Projects</h1>
+        <h1>Hello {name}!</h1>
+        <input className="project-search-bar" type="search" placeholder="Search Projects..." />
+        <h2 className="myProjects">My Projects</h2>
+        <div className="project-filters">
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </div>
       </div>
     </DashboardLayout>
   );
