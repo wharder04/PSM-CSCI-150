403138fcf318a6bcc94ad626642e81c25ec76e91
Aleyah <leilei55@mail.fresnostate.edu>
2025-11-18 19:38:58 -0800

    Added a plus icon and useStates to TaskBoard and updated the styling for page

--- client/src/pages/TaskBoard/TaskBoard.jsx
+++ client/src/pages/TaskBoard/TaskBoard.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+import { MdAdd } from "react-icons/md";
 import DashboardLayout from "./DashboardLayout";
 
 function TaskBoard () {
+    const [unAssignedTasks, setUnassignedTasks] = useState([])
+    const [assignedTasks, setAssignedTasks] = useState([])
+    const [inProgressTasks, setInProgressTasks] = useState([])
+    const [completedTasks, setCompletedTasks] = useState([])
+    const [incompleteTasks, setIncompleteTasks] = useState([])
+
     return(
         <DashboardLayout>
-            <div>
+            <div className="min-h-screen p-8 bg-gray-100 text-black">
                 <div>
                     <h2 className="text-3xl font-bold text-black mb-6">Task Board</h2>
                     <p className="text-2xl text-black font-semibold">Drag tasks to update their status</p>
@@ -11,20 +19,40 @@ function TaskBoard () {
                     <button className="bg-gray-300 text-black font-sans rounded w-32 h-10">Create New Task</button>
                 </div>
                 <div className="flex flex-row">
-                    <div className="w-32 h-96 rounded">
-                        <p className="text-black font-sans">Unassigned</p>
+                    <div className="flex flex-column border-2 border-black w-32 h-96 rounded">
+                        <div className="text-black font-sans">
+                            <p>Unassigned</p>
+                            <MdAdd size={30} color="black"/>
+                        </div>
+                        <div class="w-full border-t border-black"></div>
                     </div>
-                    <div className="w-32 h-96 rounded">
-                        <p className="text-black font-sans">Assigned</p>
+                    <div className="flex flex-column border-2 border-black w-32 h-96 rounded">
+                        <div className="text-black font-sans">
+                            <p>Assigned</p>
+                            <MdAdd size={30} color="black"/>
+                        </div>
+                        <div class="w-full border-t border-black"></div>
                     </div>
-                    <div className="w-32 h-96 rounded">
-                        <p className="text-black font-sans">In Progress</p>
+                    <div className="flex flex-column border-2 border-black w-32 h-96 rounded">
+                        <div className="text-black font-sans">
+                            <p>In Progress</p>
+                            <MdAdd size={30} color="black"/>
+                        </div>
+                        <div class="w-full border-t border-black"></div>
                     </div>
-                    <div className="w-32 h-96 rounded">
-                        <p className="text-black font-sans">Completed</p>
+                    <div className="flex flex-column border-2 border-black w-32 h-96 rounded">
+                        <div className="text-black font-sans">
+                            <p>Completed</p>
+                            <MdAdd size={30} color="black"/>
+                        </div>
+                        <div class="w-full border-t border-black"></div>
                     </div>
-                    <div className="w-32 h-96 rounded">
-                        <p className="text-black font-sans">Incomplete</p>
+                    <div className="flex flex-column border-2 border-black w-32 h-96 rounded">
+                        <div className="text-black font-sans">
+                            <p>Incomplete</p>
+                            <MdAdd size={30} color="black"/>
+                        </div>
+                        <div class="w-full border-t border-black"></div>
                     </div>
                 </div>
             </div>
