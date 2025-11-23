5ec4e01afb7a1e89428b15ed6ff9718f8a983513
Aleyah <leilei55@mail.fresnostate.edu>
2025-11-22 20:59:56 -0800

    Updated styling for Taskboard and created the page and styling for KanbanBoard

---
+++ client/src/pages/KanbanBoard/KanbanBoard.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+function KanbanBoard () {
+    const [projDescription, setProjDescription] = useState("");
+    const [totalTasks, setTotalTasks] = useState(0);
+    const [assignedTasks, setAssignedTasks] = useState(0);
+    const [incompleteTasks, setIncompleteTasks] = useState(0);
+    const [completedTasks, setCompletedTasks] = useState(0);
+    const [overdueTasks, setOverdueTasks] = useState(0);
+    const [tasks, setTasks] = useState([]);
+    const [team, setTeam] = useState([]);
+    const [activeSection, setActiveSection] = useState("tasksActive");
+    
+    return (
+        <div className="min-h-screen w-full flex flex-col p-10 bg-gray-100 text-black">
+            <div className="w-1/3 h-full p-8 flex flex-col">
+                <div className="mb-6">
+                    <p className="text-2x1 font-semibold text-black bg-gray-200">Project Description</p>
+                    <p clasName="text-2x1 text-black bg-gray-200">This is a mobile development project</p>
+                </div>
+                <div className="flex flex-row gap-4 mb-6">
+                    <div className="bg-gray-200 p-4 w-40 h-32 rounded flex flex-col">
+                        <p className="text-black font-semibold text-2x1">Total Tasks</p>
+                    </div>
+                    <div className="bg-gray-200 p-4 w-40 h-32 rounded flex flex-col">
+                        <p className="text-black font-semibold text-2x1">Assigned Tasks</p>
+                    </div>
+                    <div className="bg-gray-200 p-4 w-40 h-32 rounded flex flex-col">
+                        <p className="text-black font-semibold text-2x1">Incomplete Tasks</p>
+                    </div>
+                    <div className="bg-gray-200 p-4 w-40 h-32 rounded flex flex-col">
+                        <p className="text-black font-semibold text-2x1">Completed Tasks</p>
+                    </div>
+                    <div className="bg-gray-200 p-4 w-40 h-32 rounded flex flex-col">
+                        <p className="text-black font-semibold text-2x1">Overdue Tasks</p>
+                    </div>
+                </div>
+                <div className="mb-6">
+                    <button className="bg-gray-800 text-white font-sans rounded w-24 h-10 mb-4">+ New Task</button>
+                </div>
+                <div>
+                    <p className="text-base text-black">Overall Progress</p>
+                </div>
+            </div>
+            <div className="w-2/3 h-full flex flex-col gap-6 p-6">
+                <div className="flex flex-row gap-4 mb-4">
+                    <button className="bg-gray-200 text-black rounded px-2 py-2 text-base w-24 h-10" onClick={() => setActiveSection("tasksActive")}>Tasks</button>
+                    <button className="bg-gray-200 text-black rounded px-2 py-2 text-base w-24 h-10" onClick={() => setActiveSection("teamActive")}>Team</button>
+                </div>
+                <div className="gap-4 mb-4">
+                    {activeSection === "tasksActive" && (
+                        <div className="gap-4 mb-4">
+                            <table class="w-full table-fixed text-left">
+                                <thead class="bg-gray-100">
+                                    <tr className="h-12">
+                                        <th class="px-4 py-2 w-1/5 text-sm text-black">Name</th>
+                                        <th class="px-4 py-2 w-1/5 text-sm text-black">Assigned To</th>
+                                        <th class="px-4 py-2 w-1/5 text-sm text-black">Assignee</th>
+                                        <th class="px-4 py-2 w-1/5 text-sm text-black">Date Assigned</th>
+                                        <th class="px-4 py-2 w-1/5 text-sm text-black">Priority</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-gray-100">
+                                    <tr class="border-t border-gray-200 h-12"></tr>
+                                    <tr class="border-t border-gray-200 h-12"></tr>
+                                    <tr class="border-t border-gray-200 h-12"></tr>
+                                    <tr class="border-t border-gray-200 h-12"></tr>
+                                    <tr class="border-t border-gray-200 h-12"></tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    )}
+                    {activeSection === "teamActive" && (
+                        <div className="gap-4 mb-4">
+                            <p className="text-black text-base">Team members go here</p>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default KanbanBoard;
--- client/src/pages/TaskBoard/TaskBoard.jsx
+++ client/src/pages/TaskBoard/TaskBoard.jsx
@@ -9,62 +9,61 @@ function TaskBoard() {
   const [incompleteTasks, setIncompleteTasks] = useState([]);
 
   return (
-    <div className="flex w-full h-screen p-8 bg-gray-100 text-black">
-      <div>
-        <h2 className="text-3xl font-bold text-black mb-6">Task Board</h2>
-        <p className="text-2xl text-black font-semibold">
+    <div className="flex w-full min-h-screen p-8 bg-gray-100 text-black">
+      <div className="w-1/3 h-full p-8 flex flex-col">
+        <h2 className="text-4xl font-bold text-black mb-6">Task Board</h2>
+        <p className="text-2xl text-black font-semibold mb-6">
           Drag tasks to update their status
         </p>
-        <button className="bg-gray-800 text-white font-sans rounded w-32 h-10">
+        <button className="bg-gray-800 text-white font-sans rounded w-24 h-10 mb-4">
           Filters
         </button>
-        <button className="flex items-center gap-2 bg-gray-300 text-black font-sans rounded w-32 h-10">
-          <MdAdd size={20} color="black" />
-          Create New Task
+        <button className="bg-gray-300 text-black font-sans rounded w-24 h-10 mb-8">
+          + Create New Task
         </button>
       </div>
-      <div className="flex flex-row">
-        <div className="flex flex-col items-center border-2 border-black w-32 h-96 rounded">
+      <div className="w-2/3 h-full flex flex-row gap-6 p-6">
+        <div className="flex flex-col items-center border-2 border-black w-40 h-[32rem] rounded p-3">
           <div className="flex items-center justify-between w-full px-2 mb-2 text-black font-sans">
             <p>Unassigned</p>
-            <button className="w-7 h-7 flex items-center justify-center">
-              <MdAdd size={30} color="black" />
+            <button className="w-7 h-7 flex items-center justify-center text-black">
+              +
             </button>
           </div>
           <div className="w-full border-t border-black"></div>
         </div>
-        <div className="flex flex-col items-center border-2 border-black w-32 h-96 rounded">
+        <div className="flex flex-col items-center border-2 border-black w-40 h-[32rem] rounded p-3">
           <div className="flex items-center justify-between w-full px-2 mb-2 text-black font-sans">
             <p>Assigned</p>
-            <button className="w-7 h-7 flex items-center justify-center">
-              <MdAdd size={30} color="black" />
+            <button className="w-7 h-7 flex items-center justify-center text-black">
+              +
             </button>
           </div>
           <div className="w-full border-t border-black"></div>
         </div>
-        <div className="flex flex-col items-center border-2 border-black w-32 h-96 rounded">
+        <div className="flex flex-col items-center border-2 border-black w-40 h-[32rem] rounded p-3">
           <div className="flex items-center justify-between w-full px-2 mb-2 text-black font-sans">
             <p>In Progress</p>
-            <button className="w-7 h-7 flex items-center justify-center">
-              <MdAdd size={30} color="black" />
+            <button className="w-7 h-7 flex items-center justify-center text-black">
+              +
             </button>
           </div>
           <div className="w-full border-t border-black"></div>
         </div>
-        <div className="flex flex-col items-center border-2 border-black w-32 h-96 rounded">
+        <div className="flex flex-col items-center border-2 border-black w-40 h-[32rem] rounded p-3">
           <div className="flex items-center justify-between w-full px-2 mb-2 text-black font-sans">
             <p>Completed</p>
-            <button className="w-7 h-7 flex items-center justify-center">
-              <MdAdd size={30} color="black" />
+            <button className="w-7 h-7 flex items-center justify-center text-black">
+              +
             </button>
           </div>
           <div className="w-full border-t border-black"></div>
         </div>
-        <div className="flex flex-col items-center border-2 border-black w-32 h-96 rounded">
+        <div className="flex flex-col items-center border-2 border-black w-40 h-32[rem] rounded p-3">
           <div className="flex items-center justify-between w-full px-2 mb-2 text-black font-sans">
             <p>Incomplete</p>
-            <button className="w-7 h-7 flex items-center justify-center">
-              <MdAdd size={30} color="black" />
+            <button className="w-7 h-7 flex items-center justify-center text-black">
+              +
             </button>
           </div>
           <div className="w-full border-t border-black"></div>
