c784611fdeecb1ed44705d0c0f3f9b1065bf858f
Aleyah <leilei55@mail.fresnostate.edu>
2025-11-25 19:40:06 -0800

    Updated kanbanboard to fit with current UI design

--- client/src/pages/KanbanBoard/KanbanBoard.jsx
+++ client/src/pages/KanbanBoard/KanbanBoard.jsx
@@ -14,6 +14,15 @@ function KanbanBoard () {
     const [tasks, setTasks] = useState([]);
     const [team, setTeam] = useState([]);
     const [activeSection, setActiveSection] = useState("tasksActive");
+    const [showPopup, setShowPopup] = useState(false);
+    const [memberEmail, setMemberEmail] = useState("");
+    const [roleDropdownOpen, setRoleDropdownOpen] = useState(false);
+    const [memberDropdownOpen, setMemberDropdownOpen] = useState(false);
+    const [newTaskName, setNewTaskName] = useState("");
+    const [newTaskDescription, setNewTaskDescription] = useState("");
+    const [priorityDropdownOpen, setPriorityDropdownOpen] = useState(false);
+    const [assigneeDropdown, setAssigneeDropdown] = useState(false);
+
 
     useEffect(() => {
         setProject(projects[2]);
@@ -51,7 +60,67 @@ function KanbanBoard () {
                     </div>
                 </div>
                 <div className="mb-6">
-                    <button className="bg-gray-800 text-white font-sans rounded w-24 h-10 mb-4">+ New Task</button>
+                    <button className="bg-gray-800 text-white font-sans rounded w-24 h-10 mb-4" onClick={() => setShowPopup(true)}>+ New Task</button>
+                    {showPopup && (
+                        <div className="fixed inset-0 bg-black bg-opacity-50" onClick={() => setShowPopup(false)}></div>
+                    )}
+                    {showPopup && (
+                        <div className="fixed right-0 top-0 w-96 h-full bg-white p-6 z-50">
+                            <div className="flex flex-col">
+                                <div className=" relative flex flex-row">
+                                    <h2 className="text-black text-base font-semibold">Add New Task</h2>
+                                    <p className="absolute right-0 text-black text-sm">x</p>
+                                </div>
+                                <div className="flex flex-row">
+                                    <p className="text-base text-black mb-4">Name</p>
+                                    <input 
+                                        type="text"
+                                        value={newTaskName}
+                                        onChange={(e) =>  setNewTaskName(e.target.value)}
+                                        className="border-b-2 border-black focus:outline-none focus:border-black w-full py-1"
+                                    />
+                                </div>
+                                <div className="flex flex-row">
+                                    <p className="text-base text-black mb-4">Description</p>
+                                    <input 
+                                        type="text"
+                                        value={newTaskDescription}
+                                        onChange={(e) =>  setNewTaskDescription(e.target.value)}
+                                        className="border-b-2 border-black focus:outline-none focus:border-black w-full py-1"
+                                    />
+                                </div>
+                                <div className="flex flex-row">
+                                    <p className="text-black text-base">Priority</p>
+                                    <div className="relative inline-block">
+                                            <button className="px-4 py-2 bg-gray-200 rounded" onClick={() => setPriorityDropdownOpen(!priorityDropdownOpen)}></button>
+                                            {priorityDropdownOpen && (
+                                                <div className="absolute right-0 mt-2 w-1/4 bg-gray-200 rounded">
+                                                    <button className="block w-full text-left text-black text-base px-4 py-2 bg-gray-200 rounded">Low</button>
+                                                    <button className="block w-full text-left text-black text-base px-4 py-2 bg-gray-200 rounded">Medium</button>
+                                                    <button className="block w-full text-left text-black text-base px-4 py-2 bg-gray-200 rounded">High</button>
+                                                </div>
+                                            )}
+                                    </div>
+                                </div>
+                                <div className="flex flex-row">
+                                    <p className="text-black text-base">Assigned To</p>
+                                    <div className="relative inline-block">
+                                            <button className="px-4 py-2 bg-gray-200 rounded" onClick={() => setAssigneeDropdown(!assigneeDropdown)}></button>
+                                            {assigneeDropdown && (
+                                                <div className="absolute right-0 mt-2 w-1/4 bg-gray-200 rounded">
+                                                    {team.map(teamMember =>
+                                                        <button 
+                                                            className="block w-full text-left text-black text-base px-4 py-2 bg-gray-200 rounded"
+                                                            key={teamMember.id}
+                                                        >{teamMember.name}</button>
+                                                    )}
+                                                </div>
+                                            )}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
                 <div>
                     <p className="text-base text-black">Overall Progress</p>
@@ -90,12 +159,56 @@ function KanbanBoard () {
                         </div>
                     )}
                     {activeSection === "teamActive" && (
-                        <div className="flex flex-row gap-4 mb-4 justify-start">
-                            {team.map(teamMember => (
-                                <div className ="w-40 h-32 bg-white rounded mb-4" key={teamMember.id}>
-                                    <p className="text-black font-semibold mb-4">{teamMember.name}</p>
+                        <form className="w-2/3 h-1/2 flex flex-col border-2 border-black bg-white">
+                            <p className="text-base font-semibold text-black p-2">Add New Member</p>
+                            <p className="text-sm text-black p-2">Invite team member using their email id</p>
+                            <div className="flex flex-row mb-4">
+                                <div className="w-[60%] flex flex-col mb-8 text-white">
+                                    <label className="font-medium">Email Address:</label>
+                                        <input
+                                            type="text"
+                                            id="email"
+                                            name="email"
+                                            value={memberEmail}
+                                            onChange={(e) => setMemberEmail(e.target.value)}
+                                            className="px-4 py-3 rounded-xl bg-gray-700"
+                                        />
                                 </div>
-                            ))}
+                                <div className="flex flex-col mb-4">
+                                    <p className="text-black text-base px-4 py-2">Role</p>
+                                    <div className="relative inline-block">
+                                        <button className="px-4 py-2 bg-gray-200 rounded" onClick={() => setRoleDropdownOpen(!roleDropdownOpen)}></button>
+                                        {roleDropdownOpen && (
+                                            <div className="absolute mt-2 w-1/4 bg-gray-200 rounded">
+                                                <button className="block w-full text-left text-black text-base px-4 py-2">Admin</button>
+                                                <button className="block w-full text-left text-black text-base px-4 py-2">Member</button>
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                            <button className="w-1/3 h-1/5 bg-gray-200 text-black text-base">+ Add Member</button>
+                        </form>
+                    )}
+                    {activeSection === "teamActive" && (
+                        <div className="bg-white w-xl h-64 flex flex-col mb-4">
+                            <p className="bg-white text-base text-black mb-4">Team Members</p>
+                            <div className="flex flex-col gap-4 mb-4">
+                                {team.map(teamMember => (
+                                    <div className ="w-xl h-24 bg-white rounded mb-4 flex flex-row items-center justify-between px-4" key={teamMember.id}>
+                                        <p className="text-black font-semibold">{teamMember.name}</p>
+                                        <div className="relative inline-block">
+                                            <button className="px-4 py-2 bg-gray-200 rounded text-base text-black" onClick={() => setMemberDropdownOpen(!memberDropdownOpen)}>Select Role</button>
+                                            {memberDropdownOpen && (
+                                                <div className="absolute right-0 mt-2 w-1/4 bg-gray-200 rounded">
+                                                    <button className="block w-full text-left text-black text-base px-4 py-2">Admin</button>
+                                                    <button className="block w-full text-left text-black text-base px-4 py-2">Member</button>
+                                                </div>
+                                             )}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     )}
                 </div>
