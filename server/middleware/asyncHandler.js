efdd648dc8261272d0f1b4ff1031f52c9401bed7
wharder04 <wharder@mail.fresnostate.edu>
2026-03-13 11:20:00 -0700

    Delete server directory

--- server/middleware/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// Wrap async route handlers to forward errors to the error middleware
-export default function asyncHandler(fn) {
-  return function wrapped(req, res, next) {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-}
